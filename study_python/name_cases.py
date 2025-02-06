6d6f3f3411d50db4025145ee6affc775ebcc0857
HanPlayHard <hanplayhard@foxmail.com>
2025-02-06 22:33:32 +0800

    Practice Python basics

---
+++ study_python/name_cases.py
@@ -0,0 +1,12 @@
+user_name = "Hello man, would you like to learn some Python today?"
+print(user_name)
+
+name1 = "han play hard"
+print(name1.lower())
+print(name1.upper())
+print(name1.title())
+
+name2 = " John Johnson "
+quote = "Great works not by strength, but by insist to complete."
+message = "\t" + name2.strip() + " once said, \n" + '"' + quote + '"'
+print(message)
