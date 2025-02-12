93f57cd2a359f378d5dc783442dd2388243e117c
HanPlayHard <hanplayhard@foxmail.com>
2025-02-12 22:50:47 +0800

    Project 1, Alien invasion 1.

---
+++ study_python/Project/project_description.py
@@ -0,0 +1,26 @@
+# Project Description:
+# This folder holds the complete project
+"""
+
+plugin: Better Comments
+! 辅助方法（helper method）一般只在类中调
+! 用，不会在类外调用。在 Python 中，辅助方法的名称以单下划线打头。
+! helper methods are usually called only within a class
+! It will not be called outside the class. 
+! In Python, helper method names start with a single underscore.
+* 123
+? 123
+// 123
+\\ 1231
+todo Actively learn python
+"""
+
+
+"""
+Project 1 Alien invasion (game, Pygame)
+
+python -m pip install --user pygame
+or 
+python -m pip install  pygame
+
+"""
