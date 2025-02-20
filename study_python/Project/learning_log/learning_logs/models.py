3eaca4ab05fa5af3b86f6e97ec7848f944b6a191
HanPlayHard <hanplayhard@foxmail.com>
2025-02-20 15:12:07 +0800

    Project 2, learning_log 2: simple web application

--- study_python/Project/learning_log/learning_logs/models.py
+++ study_python/Project/learning_log/learning_logs/models.py
@@ -12,3 +12,17 @@ class Topic(models.Model):
         """返回模型的字符串表示"""
         # 每当需要生成 表示模型实例 的输出时，Django 都将调用这个方法
         return self.text
+
+class Entry(models.Model):
+    """学到的有关某个主题的具体知识"""
+    # 外键，引用另一个表的主键 , 级联删除（cascading delete）
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE) 
+    text = models.TextField()  # TextField——文本数据，不限制长度
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+    
+    def __str__(self):
+        """返回一个表示条目的简单字符串"""
+        return f"{self.text[:50]}..."
